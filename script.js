9b125057613114747375d8b0fa4b554f3893c26e
codelotusr <smaliukasdeividas@gmail.com>
2024-07-28 15:02:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,254 @@
+var cheatsheets = [
+	`
+    <h2>Data Structures</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Data Structure</th>
+                <th colspan="8">Time Complexity</th>
+                <th>Space Complexity</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th colspan="4">Average</th>
+                <th colspan="4">Worst</th>
+                <th>Worst</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Array</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+            <tr>
+                <td>Stack</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+            <tr>
+                <td>Queue</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+            <tr>
+                <td>Singly-Linked List</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+            <tr>
+                <td>Doubly-Linked List</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+            <tr>
+                <td>Hash Table</td>
+                <td>-</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td class="green">O(1)</td>
+                <td>-</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+                <td class="yellow">O(n)</td>
+            </tr>
+
+
+        </tbody>
+    </table>
+    `,
+	`
+    <h2>Sorting Algorithms</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Sorting Algorithms</th>
+                <th colspan="8">Time Complexity</th>
+                <th>Space Complexity</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th colspan="4">Average</th>
+                <th colspan="4">Worst</th>
+                <th>Worst</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th></th>
+            </tr>
+        </thead>
+    `,
+	`
+    <h2>Sorting Algorithms</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Sorting Algorithms</th>
+                <th colspan="8">Time Complexity</th>
+                <th>Space Complexity</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th colspan="4">Average</th>
+                <th colspan="4">Worst</th>
+                <th>Worst</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th></th>
+            </tr>
+        </thead>
+    `,
+	`
+    <h2>Sorting Algorithms</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Sorting Algorithms</th>
+                <th colspan="8">Time Complexity</th>
+                <th>Space Complexity</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th colspan="4">Average</th>
+                <th colspan="4">Worst</th>
+                <th>Worst</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th></th>
+            </tr>
+        </thead>
+    `,
+	`
+    <h2>Sorting Algorithms</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Sorting Algorithms</th>
+                <th colspan="8">Time Complexity</th>
+                <th>Space Complexity</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th colspan="4">Average</th>
+                <th colspan="4">Worst</th>
+                <th>Worst</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th>Indexing</th>
+                <th>Search</th>
+                <th>Insertion</th>
+                <th>Deletion</th>
+                <th></th>
+            </tr>
+        </thead>
+    `,
+];
+
+var currentIndex = 0;
+
+function nextImage() {
+	currentIndex = (currentIndex + 1) % cheatsheets.length;
+	document.getElementById("cheatsheet").innerHTML = cheatsheets[currentIndex];
+}
+
+function previousImage() {
+	currentIndex = (currentIndex - 1 + cheatsheets.length) % cheatsheets.length;
+	document.getElementById("cheatsheet").innerHTML = cheatsheets[currentIndex];
+}
+
+document.getElementById("cheatsheet").innerHTML = cheatsheets[currentIndex];
+
+function updateClock() {
+	var now = new Date();
+	var hours = String(now.getHours()).padStart(2, "0");
+	var minutes = String(now.getMinutes()).padStart(2, "0");
+	var seconds = String(now.getSeconds()).padStart(2, "0");
+	var day = String(now.getDate()).padStart(2, "0");
+	var month = String(now.getMonth() + 1).padStart(2, "0");
+	var year = now.getFullYear();
+	var formattedTime = `${hours}:${minutes}:${seconds} ${day}:${month}:${year}`;
+	document.getElementById("clock").innerText = formattedTime;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
